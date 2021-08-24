4d1ea4da785e80fffba3e5f07bbaedac5c4208de
lohsea <915374238@qq.com>
2021-08-24 18:06:24 +0800

    feat(page): init

---
+++ declarations.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Author: lixin
+ * @Date: 2021-08-16 19:01:12
+ * @LastEditTime: 2021-08-24 17:29:18
+ */
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
