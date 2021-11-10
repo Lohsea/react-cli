bf7e891d56afd63882a0136522efd5bcf59a35b6
lohsea <915374238@qq.com>
2021-11-10 14:53:14 +0800

    删除多余文件，增加reset.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,19 @@
  * @Description:
  * @Author: lixin
  * @Date: 2021-08-06 11:23:24
- * @LastEditTime: 2021-08-30 16:53:03
+ * @LastEditTime: 2021-11-10 14:46:58
  */
 
 import React from "react";
 import ReactDOM from "react-dom";
 // import * as wasm from "starks_proofgen";
-import config from "./config/config.json";
-
-import Api from "./components/Api/Api";
 import HomePage from "./pages/homePage";
-import EventQueue from "./components/Event/EventQueue";
 
 import "./styles/index.scss";
 import "antd/dist/antd.css";
 
 function App(): React.ReactElement {
-  return (
-    <Api url={config.url}>
-      {/* <EventQueue> */}
-      <HomePage />
-      {/* </EventQueue> */}
-    </Api>
-  );
+  return <HomePage />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
